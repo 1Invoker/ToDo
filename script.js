d52dc73ff60673681c1b3abdd237ee5cc831004f
1Invoker <sovun000@gmail.com>
2023-05-06 18:39:26 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,48 +1,93 @@
-// 1) Создаем пустой массив
-const tasks = [{id: 1, task: 'vsvsvsvs'}];
+// Объявляем переменную tasks и присваиваем ей массив объектов-задач, с полем id, task и delete
+const tasks = [{id: 1, task: 'vsvsvsvs', delete: false}];
+// Объявляем переменную button и находим на странице элемент с классом button
 const button = document.querySelector('.button');
+// Объявляем переменную input и находим на странице элемент с классом input
 const input = document.querySelector('.input');
+// Объявляем переменную ul и находим на странице элемент с классом ul
 const ul = document.querySelector('.ul');
+// Объявляем переменную deleteButton и находим на странице элемент с классом delete-button
+const deleteButton = document.querySelector('.delete-button');
+
+// Функция, которая отрисовывает задачи на странице
 function renderTasks() {
-  for (let i = 0; i < tasks.length; i++) {
-    const li = document.createElement('li');
-    li.innerHTML = tasks[i].task;
-    ul.append(li);
-  }
+// Очищаем список перед отрисовкой, чтобы избежать дублирования элементов
+ul.innerHTML = '';
+// Проходим циклом по массиву tasks и создаем для каждой задачи элемент списка
+for (let i = 0; i < tasks.length; i++) {
+// Создаем элемент li
+const li = document.createElement('li');
+// Создаем чекбокс и добавляем ему класс checkbox и атрибут data-id со значением id текущей задачи
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+checkbox.classList.add('checkbox');
+checkbox.dataset.id = tasks[i].id;
+// Добавляем в элемент li текст задачи и атрибут data-id со значением id текущей задачи
+li.innerHTML = tasks[i].task;
+li.dataset.id = tasks[i].id;
+// Добавляем в элемент li класс task-item и вставляем перед текстом чекбокс
+li.classList.add('task-item');
+li.prepend(checkbox);
+// Добавляем элемент li в список ul
+ul.append(li);
 }
+}
+
+// Вызываем функцию renderTasks, чтобы отрисовать задачи при загрузке страницы
 renderTasks();
 
+// Обработчик события нажатия на кнопку "Добавить задачу"
 button.onclick = () => {
-  addTask(input.value, tasks)
-  // renderTasks()
+// Вызываем функцию addTask и передаем ей текст новой задачи и массив tasks
+addTask(input.value, tasks);
+// Вызываем функцию renderTasks, чтобы отрисовать задачи с учетом новой задачи
+renderTasks();
 }
 
+// Обработчик события нажатия на кнопку "Удалить задачи"
+deleteButton.onclick = () => {
+// Вызываем функцию deleteTasks, чтобы удалить задачи, у которых значение delete равно true
+deleteTasks();
+// Вызываем функцию renderTasks, чтобы отрисовать задачи после удаления
+renderTasks();
+}
 
-// 2) Функция для создания id элемента массива
+// Функция, которая генерирует уникальный id для новой задачи
 function generateId(arr) {
- let id = 1;
- for(let i = 1; i < tasks.length ; i++){
-  const element = arr[i];
-  if (element.id == id){
-    id++;
-  }
- }
- return(id);
+let id = 1;
+for(let i = 1; i < tasks.length ; i++){
+const element = arr[i];
+if (element.id == id){
+id++;
 }
-// 3) Функция для добавления задачи в массив
+}
+return(id);
+}
+// Функция для добавления новой задачи в массив с id, текстом задачи и флагом delete
 function addTask(text, array) {
   const task = {
-    id: generateId(array),
-    task: text
+  id: generateId(array), // генерируем уникальный id для задачи
+  task: text, // текст задачи
+  delete: false // флаг, который показывает, нужно ли удалять эту задачу
   };
-  array.push(task);
-}
-
-//4) Создать функцтю для удаления одной задачи которая принимает параметром id этой задачи
-function deleted(id) {
-  tasks = tasks.filter(element => element.id !== id);
-
-}
+  array.push(task); // добавляем задачу в массив
+  }
+  
+  // Функция для удаления задач из массива, у которых флаг delete равен true
+  function deleteTasks() {
+  tasks = tasks.filter(task => !task.delete); // фильтруем массив задач, оставляя только те, у которых delete равен false
+  }
+  
+  // Обработчик событий на ul, который реагирует на клик на чекбоксе и помечает задачу на удаление
+  ul.addEventListener('click', (event) => {
+  if (event.target.classList.contains('checkbox')) { // если кликнули на чекбокс
+  const taskId = parseInt(event.target.dataset.id); // получаем id задачи из атрибута data-id у чекбокса
+  const taskItem = document.querySelector(`[data-id="${taskId}"]`); // находим элемент li с задачей
+  const taskIndex = tasks.findIndex(task => task.id === taskId); // находим индекс задачи в массиве по её id
+  tasks[taskIndex].delete = event.target.checked; // помечаем задачу на удаление, если чекбокс отмечен
+  taskItem.classList.toggle('checked'); // добавляем или удаляем класс 'checked' у элемента li в зависимости от состояния чекбокса
+  }
+  });
 //==========================================================================================================
 
 
